cebfa3087fb90a92d52c145b1092aed4761414b5
GeetaPogul <87964049+GeetaPogul@users.noreply.github.com>
2021-08-19 19:08:55 +0530

    error handler

--- app.js
+++ app.js
@@ -1,14 +1,33 @@
 
 
-var btn = document.querySelector("#btn1");
 
-btn.addEventListener("click",function clickEventHandler() {
 
-    console.log("clicked")
+
+var button=document.querySelector("#btn");
+
+var txtInput=document.querySelector("#txt-input");
+
+var outputDiv=document.querySelector("#output");
+
+var serverURL = "https://api.funtranslations.com/translate/minion.json";
+
+function getTranslationURL(text){
+    return serverURL +"?"+"text="+ text;
+}
+
+function errorHandler(error){
+    console.log("error occured",error);
+    alert("something wrong with the server ! try again after sometime");
+}
+
+button.addEventListener("click",function(){
+    // console.log("button has clicked")
+    var userText  =txtInput.value;
+    fetch(getTranslationURL(userText))
+    .then(response => response.json())
+    .then( json =>{
+        var translatedText = json.contents.translated;
+        outputDiv.innerText =translatedText;
+    })
+    .catch(errorHandler);
 })
-
-    
-
-
-
-    
